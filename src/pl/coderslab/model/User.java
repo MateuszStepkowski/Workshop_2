c00e71c607633a913ab73853b8fce8d64482bea7
MateuszStępkowski <stepkowski.mat@gmail.com>
2018-07-29 00:54:02 +0200

    User class complement & DataBase Tables creation

--- src/pl/coderslab/model/User.java
+++ src/pl/coderslab/model/User.java
@@ -10,24 +10,21 @@ import java.util.ArrayList;
 
 
 public class User {
-    private int id;
-    private String name;
+    private long id;
     private String username;
     private String password;
     private String email;
+    private int user_group_id;
 
-    public User(String name, String username, String password, String email) {
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.email = email;
+    public User(String username, String password, String email, int user_group_id) {
+
+        setUsername(username);
+        setPassword(password);
+        setEmail(email);
+        setUser_group_id(user_group_id);
     }
 
     public User() {
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public void setUsername(String username) {
@@ -42,18 +39,49 @@ public class User {
         this.email = email;
     }
 
+    public void setUser_group_id(int user_group_id) {
+        this.user_group_id = user_group_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getUser_group_id() {
+        return user_group_id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", user_group_id=" + user_group_id +
+                '}';
+    }
 
     public void saveToDB(){
 
         if(this.id==0){
             try {
-                String sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+                String sql = "INSERT INTO users(username, email, password, user_group_id) VALUES (?, ?, ?, ?)";
                 String generatedColumns[] = {"ID"};
                 PreparedStatement preparedStatement;
                 preparedStatement = DbManager.getInstance().getConnection().prepareStatement(sql, generatedColumns);
                 preparedStatement.setString(1, this.username);
                 preparedStatement.setString(2, this.email);
                 preparedStatement.setString(3, this.password);
+                preparedStatement.setInt(4, this.user_group_id);
                 preparedStatement.executeUpdate();
                 ResultSet rs = preparedStatement.getGeneratedKeys();
                 if (rs.next()) {
@@ -63,13 +91,14 @@ public class User {
 
         }else{
             try{
-                String sql = "UPDATE users SET username=?, email=?, password=? where id = ?";
+                String sql = "UPDATE users SET username=?, email=?, password=?, user_group_id=? where id = ?";
                 PreparedStatement preparedStatement;
                 preparedStatement = DbManager.getInstance().getConnection().prepareStatement(sql);
                 preparedStatement.setString(1, this.username);
                 preparedStatement.setString(2, this.email);
                 preparedStatement.setString(3, this.password);
-                preparedStatement.setInt(4, this.id);
+                preparedStatement.setInt(4, this.user_group_id);
+                preparedStatement.setLong(5, this.id);
                 preparedStatement.executeUpdate();
             }catch (SQLException e){e.printStackTrace();}
         }
@@ -79,7 +108,7 @@ public class User {
             String sql = "DELETE FROM users WHERE id = ?";
             PreparedStatement preparedStatement;
             preparedStatement = DbManager.getInstance().getConnection().prepareStatement(sql);
-            preparedStatement.setInt(1, this.id);
+            preparedStatement.setLong(1, this.id);
             preparedStatement.executeUpdate();
             this.id=0;
         }catch (SQLException e){e.printStackTrace();}
@@ -98,6 +127,7 @@ public class User {
                 user.username = rs.getString("username");
                 user.email = rs.getString("email");
                 user.password = rs.getString("password");
+                user.user_group_id = rs.getInt("users_group_id");
                 return user;
             }
         }catch (SQLException e){e.printStackTrace();}
@@ -116,10 +146,34 @@ public class User {
                 user.username = rs.getString("username");
                 user.email = rs.getString("email");
                 user.password = rs.getString("password");
+                user.user_group_id = rs.getInt("users_group_id");
+
                 users.add(user);
             }
             return users;
         }catch (SQLException e){e.printStackTrace();}
+        return null;
+    }
+
+    public static ArrayList<User> loadAllByGroupId(int groupId) {
+        try {
+            ArrayList<User> users = new ArrayList<>();
+            String sql = "SELECT id, username, email, password, users_group_id FROM users WHERE user_group_id = ?";
+            PreparedStatement preparedStatement;
+            preparedStatement = DbManager.getInstance().getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, groupId);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()) {
+                User user = new User();
+                user.id = rs.getLong("id");
+                user.username = rs.getString("username");
+                user.password = rs.getString("password");
+                user.email = rs.getString("email");
+                user.user_group_id = rs.getInt("users_group_id");
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) { e.printStackTrace(); }
         return null;
     }
 }
---
+++ src/pl/coderslab/sql/servicedTables.java
@@ -0,0 +1,36 @@
+package pl.coderslab.sql;
+
+public class servicedTables {
+
+    String user_group_Table =
+            "CREATE TABLE user_group(" +
+                    "  id INT(11) NOT NULL AUTO_INCREMENT," +
+                    "  name VARCHAR(255)," +
+                    "  PRIMARY KEY(id));";
+    String users_Table =
+            "CREATE TABLE users("+
+            "  id BIGINT(20) NOT NULL AUTO_INCREMENT,"+
+            "  username VARCHAR(255),"+
+            "  email VARCHAR(255) UNIQUE,"+
+            "  password VARCHAR(255),"+
+            "  user_group_id INT(11),"+
+            "  PRIMARY KEY(id),"+
+            "  FOREIGN KEY(user_group_id) REFERENCES user_group(id));";
+    String excercise_Table =
+            "CREATE TABLE excercise(" +
+                    "  id INT(11) NOT NULL AUTO_INCREMENT," +
+                    "  title VARCHAR(255)," +
+                    "  description TEXT," +
+                    "  PRIMARY KEY(id));";
+    String solution_Table =
+            "CREATE TABLE solution(" +
+                    "  id INT(11) NOT NULL AUTO_INCREMENT," +
+                    "  created DATETIME," +
+                    "  updated DATETIME," +
+                    "  description TEXT," +
+                    "  excercise_id INT(11)," +
+                    "  users_id BIGINT(20)," +
+                    "  PRIMARY KEY(id)," +
+                    "  FOREIGN KEY(excercise_id) REFERENCES excercise(id)," +
+                    "  FOREIGN KEY(users_id) REFERENCES users(id));";
+}
